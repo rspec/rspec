f5cb8cfbe9506eefd9ad500cb9d5ed5134678680
Phil Pirozhkov <hello@fili.pp.ru>
2021-02-16 23:34:42 +0300

    Use :skip instead of :if/:else in specs
    Related to https://github.com/rspec/rspec-core/issues/1821

--- rspec-core/spec/rspec/core/formatters/html_formatter_spec.rb
+++ rspec-core/spec/rspec/core/formatters/html_formatter_spec.rb
@@ -41,7 +41,7 @@ module RSpec
 
         # Uncomment this group temporarily in order to overwrite the expected
         # with actual.  Use with care!!!
-        describe "file generator", :if => ENV['GENERATE'] do
+        describe "file generator", :skip => !ENV['GENERATE'] do
           it "generates a new comparison file" do
             Dir.chdir(root) do
               File.open(expected_file, 'w') {|io| io.write(actual_html)}
@@ -56,7 +56,7 @@ module RSpec
             grep(/formatter_specs\.rb/)
         end
 
-        describe 'produced HTML', :if => RUBY_VERSION <= '2.0.0' do
+        describe 'produced HTML', :skip => RUBY_VERSION > '2.0.0' do
           # Rubies before 2 are a wild west of different outputs, and it's not
           # worth the effort to maintain accurate fixtures for all of them.
           # Since we are verifying fixtures on other rubies, if this code at
@@ -67,7 +67,7 @@ module RSpec
           end
         end
 
-        describe 'produced HTML', :slow, :if => RUBY_VERSION >= '2.0.0' do
+        describe 'produced HTML', :slow, :skip => RUBY_VERSION < '2.0.0' do
           it "is identical to the one we designed manually", :pending => (defined?(RUBY_ENGINE) && RUBY_ENGINE == 'jruby') do
             expect(actual_html).to eq(expected_html)
           end
