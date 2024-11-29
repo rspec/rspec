rspec/rspec-mocks

rspec-expectations #1065: add support for a_block expectation matcher * cucumber feature passing * happy path unit specs passing * need help fixing sad path unit specs, currently marked pending

lib/rspec/mocks/argument_list_matcher.rb
@@ -54,8 +54,13 @@ module RSpec
       # position of the arguments passed to `new`.
       #
       # @see #initialize
-      def args_match?(*args)
-        Support::FuzzyMatcher.values_match?(resolve_expected_args_based_on(args), args)
+      def args_match?(*args, &block)
+        expected_args = resolve_expected_args_based_on(args)
+        if [ArgumentMatchers::BlockMatcher::INSTANCE] == expected_args
+          ArgumentMatchers::BlockMatcher::INSTANCE === block
+        else
+          Support::FuzzyMatcher.values_match?(expected_args, args)
+        end
       end
 
       # @private

rspec/rspec

rspec-mocks/lib/rspec/mocks/argument_list_matcher.rb
@@ -55,7 +55,7 @@ module RSpec
       # position of the arguments passed to `new`.
       #
       # @see #initialize
-      def args_match?(*actual_args)
+      def args_match?(*actual_args, &block)
         expected_args = resolve_expected_args_based_on(actual_args)
 
         return false if expected_args.size != actual_args.size
@@ -71,7 +71,11 @@ module RSpec
           end
         end
 
-        Support::FuzzyMatcher.values_match?(expected_args, actual_args)
+        if [ArgumentMatchers::BlockMatcher::INSTANCE] == expected_args
+          ArgumentMatchers::BlockMatcher::INSTANCE === block
+        else
+          Support::FuzzyMatcher.values_match?(expected_args, actual_args)
+        end
       end
       ruby2_keywords :args_match? if respond_to?(:ruby2_keywords, true)
 