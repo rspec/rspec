rspec/rspec-expectations

Remove lambda default parameter to maintain ruby compatability

lib/rspec/matchers/aliased_matcher.rb
@@ -34,7 +34,7 @@ module RSpec
       #
       # @api private
       def description
-        @description_block.call(super)
+        @description_block.call(super, false)
       end
 
       # Provides the failure_message of the aliased matcher. Aliased matchers
@@ -44,7 +44,7 @@ module RSpec
       #
       # @api private
       def failure_message
-        @description_block.call(super, replace_after_start: true)
+        @description_block.call(super, true)
       end
 
       # Provides the failure_message_when_negated of the aliased matcher. Aliased matchers
@@ -54,7 +54,7 @@ module RSpec
       #
       # @api private
       def failure_message_when_negated
-        @description_block.call(super, replace_after_start: true)
+        @description_block.call(super, true)
       end
     end
 
@@ -105,7 +105,7 @@ module RSpec
       def optimal_failure_message(same, inverted)
         if DefaultFailureMessages.has_default_failure_messages?(@base_matcher)
           base_message = @base_matcher.__send__(same)
-          overriden    = @description_block.call(base_message, replace_after_start: true)
+          overriden    = @description_block.call(base_message, true)
           return overriden if overriden != base_message
         end
 

rspec/rspec

rspec-expectations/lib/rspec/matchers/aliased_matcher.rb
@@ -36,7 +36,7 @@ module RSpec
       #
       # @api private
       def description
-        @description_block.call(super)
+        @description_block.call(super, false)
       end
 
       # Provides the failure_message of the aliased matcher. Aliased matchers
@@ -46,7 +46,7 @@ module RSpec
       #
       # @api private
       def failure_message
-        @description_block.call(super, replace_after_start: true)
+        @description_block.call(super, true)
       end
 
       # Provides the failure_message_when_negated of the aliased matcher. Aliased matchers
@@ -56,7 +56,7 @@ module RSpec
       #
       # @api private
       def failure_message_when_negated
-        @description_block.call(super, replace_after_start: true)
+        @description_block.call(super, true)
       end
     end
 
@@ -107,7 +107,7 @@ module RSpec
       def optimal_failure_message(same, inverted)
         if DefaultFailureMessages.has_default_failure_messages?(@base_matcher)
           base_message = @base_matcher.__send__(same)
-          overridden    = @description_block.call(base_message, replace_after_start: true)
+          overridden    = @description_block.call(base_message, true)
           return overridden if overridden != base_message
         end
 