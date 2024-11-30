rspec/rspec-expectations

Remove keyword arguments for backwards compatibility

lib/rspec/matchers/aliased_matcher.rb
@@ -44,7 +44,7 @@ module RSpec
       #
       # @api private
       def failure_message
-        @description_block.call(super, replace_after_start: true)
+        @description_block.call(super, true)
       end
 
       # Provides the failure_message_when_negated of the aliased matcher. Aliased matchers
@@ -54,7 +54,7 @@ module RSpec
       #
       # @api private
       def failure_message_when_negated
-        @description_block.call(super, replace_after_start: true)
+        @description_block.call(super, true)
       end
     end
 
@@ -105,7 +105,7 @@ module RSpec
       def optimal_failure_message(same, inverted)
         if DefaultFailureMessages.has_default_failure_messages?(@base_matcher)
           base_message = @base_matcher.__send__(same)
-          overriden    = @description_block.call(base_message, replace_after_start: true)
+          overriden    = @description_block.call(base_message, true)
           return overriden if overriden != base_message
         end
 
@@ -127,7 +127,7 @@ module RSpec
       # Creates the overriden description.
       #
       # @api private
-      def call(old_desc, replace_after_start: false)
+      def call(old_desc, replace_after_start=false)
         if @override_block
           @override_block.call(old_desc)
         else

rspec/rspec

rspec-expectations/lib/rspec/matchers/aliased_matcher.rb
@@ -46,7 +46,7 @@ module RSpec
       #
       # @api private
       def failure_message
-        @description_block.call(super, replace_after_start: true)
+        @description_block.call(super, true)
       end
 
       # Provides the failure_message_when_negated of the aliased matcher. Aliased matchers
@@ -56,7 +56,7 @@ module RSpec
       #
       # @api private
       def failure_message_when_negated
-        @description_block.call(super, replace_after_start: true)
+        @description_block.call(super, true)
       end
     end
 
@@ -107,7 +107,7 @@ module RSpec
       def optimal_failure_message(same, inverted)
         if DefaultFailureMessages.has_default_failure_messages?(@base_matcher)
           base_message = @base_matcher.__send__(same)
-          overridden    = @description_block.call(base_message, replace_after_start: true)
+          overridden    = @description_block.call(base_message, true)
           return overridden if overridden != base_message
         end
 
@@ -129,7 +129,7 @@ module RSpec
       # Creates the overriden description.
       #
       # @api private
-      def call(old_desc, replace_after_start: false)
+      def call(old_desc, replace_after_start=false)
         if @override_block
           @override_block.call(old_desc)
         else