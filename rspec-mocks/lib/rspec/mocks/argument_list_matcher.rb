p-mongo/rspec-mocks

Improve specs for a_block matcher and handle more edgecases

lib/rspec/mocks/argument_list_matcher.rb
@@ -56,8 +56,8 @@ module RSpec
       # @see #initialize
       def args_match?(*args, &block)
         expected_args = resolve_expected_args_based_on(args)
-        if [ArgumentMatchers::BlockMatcher::INSTANCE] == expected_args
-          ArgumentMatchers::BlockMatcher::INSTANCE === block
+        if ArgumentMatchers::BlockMatcher::INSTANCE == expected_args.last
+          Support::FuzzyMatcher.values_match?(expected_args, args + [block])
         else
           Support::FuzzyMatcher.values_match?(expected_args, args)
         end

rspec/rspec

rspec-mocks/lib/rspec/mocks/argument_list_matcher.rb
@@ -71,8 +71,8 @@ module RSpec
           end
         end
 
-        if [ArgumentMatchers::BlockMatcher::INSTANCE] == expected_args
-          ArgumentMatchers::BlockMatcher::INSTANCE === block
+        if ArgumentMatchers::BlockMatcher::INSTANCE == expected_args.last
+          Support::FuzzyMatcher.values_match?(expected_args, actual_args + [block])
         else
           Support::FuzzyMatcher.values_match?(expected_args, actual_args)
         end