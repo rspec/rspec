144d5b1050fcb4b052ff15008fa1e67142ccfe02
Phil Pirozhkov <hello@fili.pp.ru>
2025-07-12 22:13:36 +0300

    tmp! enable and fail on warnings

--- rspec-expectations/spec/spec_helper.rb
+++ rspec-expectations/spec/spec_helper.rb
@@ -77,6 +77,10 @@ RSpec.configure do |config|
 
   config.disable_monkey_patching!
 
+  Warning[:deprecated] = true
+  config.raise_on_warning = true
+  config.raise_errors_for_deprecations!
+
   # We don't want rspec-core to look in our `lib` for failure snippets.
   # When it does that, it inevitably finds this line:
   # `RSpec::Support.notify_failure(RSpec::Expectations::ExpectationNotMetError.new message)`
